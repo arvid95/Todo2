d8e577bbfcc6a74c9eacb1282f3ff2c293944a71
Arvid <axar1802@student.ju.se>
2023-01-25 11:38:35 +0100

    Initial Commit

---
+++ Todo2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Todo2
+//
+//  Created by Arvid Axelsson on 2023-01-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo2/Todo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo2App.swift
+//  Todo2
+//
+//  Created by Arvid Axelsson on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Todo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
